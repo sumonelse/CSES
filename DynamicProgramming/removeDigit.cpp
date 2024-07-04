3b2c8729cf828e4d42ba585c75c8f3327c4f2266
sumonelse <sumonelse01@gmail.com>
2024-07-04 11:42:49 +0530

    Solved being GREEDY

---
+++ DynamicProgramming/removeDigit.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int getMax(int n){
+    int maxi = 0;
+    while(n > 0){
+        maxi = max(maxi, n % 10);
+        n /= 10;
+    }
+    return maxi;
+}
+
+int findMin(int n){
+    if(n == 0) return 0;
+
+    // cout << "n: " << n << endl;
+
+    int maxi = getMax(n);
+    int ans = findMin(n - maxi) + 1;
+    return ans;
+}
+
+int main(){
+    // GREEDY SOLUTION
+    int n;
+    cin >> n;
+
+    cout << findMin(n) << endl;
+}
