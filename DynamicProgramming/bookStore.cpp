f0636d6a407b63d8dd6417805faa7eed52099132
sumonelse <sumonelse01@gmail.com>
2024-07-06 19:06:37 +0530

    Done with DP[SPACE-OPTIMIZATION] also

--- DynamicProgramming/bookStore.cpp
+++ DynamicProgramming/bookStore.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    // SMART-BRUTE-FORCE[DP(TABULATION)]
+    // SMART-BRUTE-FORCE[DP(SPACE-OPTIMIZATION)]
     /*
     The Question based on 0/1 KNAPSACK(BOUNDED) as we can buy the book at max ONCE.
     Observations:
@@ -20,28 +20,29 @@ int main(){
         cin >> pages[i];
     }
 
-    vector<vector<int>> dp(n, vector<int>(x + 1, 0));
+    vector<int> prev(x + 1, 0);
     // dp[i][x] represents maximum pages that we can from '0' to 'i' having amount 'x'
 
     // Handling the BASE-CASE
     for(int budget = 1; budget <= x; budget++){
         if(price[0] <= budget){
-            dp[0][budget] = pages[0];
+            prev[budget] = pages[0];
         }
     }
 
     for(int i = 1; i < n; i++){
-        dp[i][0] = 0;
+        vector<int> curr(x + 1);
+        curr[0] = 0;
         for(int budget = 1; budget <= x; budget++){
-            int notTake = dp[i - 1][budget];
+            int notTake = prev[budget];
             int take = 0;
             if(price[i] <= budget){
-                take = pages[i] + dp[i - 1][budget - price[i]];
+                take = pages[i] + prev[budget - price[i]];
             }
-            dp[i][budget] = max(notTake, take);
+            curr[budget] = max(notTake, take);
         }
+        prev = curr;
     }
 
-
-    cout << dp[n-1][x] << endl;
+    cout << prev[x] << endl;
 }
